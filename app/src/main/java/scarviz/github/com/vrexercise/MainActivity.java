5a149ca0ee47dd97c41a27da48e3da4ac53f862b
scarviz <scarviz@gmail.com>
2017-03-11 17:37:49 +0900

    AndroidThingsプロジェクト追加

---
+++ app/src/main/java/scarviz/github/com/vrexercise/MainActivity.java
@@ -0,0 +1,53 @@
+package scarviz.github.com.vrexercise;
+
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.ipadress);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            tv.setText(getIPAddress());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private String getIPAddress() {
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        String strIPAddess =
+                ((ipAddress >> 0) & 0xFF) + "." +
+                        ((ipAddress >> 8) & 0xFF) + "." +
+                        ((ipAddress >> 16) & 0xFF) + "." +
+                        ((ipAddress >> 24) & 0xFF);
+        Log.v("IP Address", strIPAddess);
+        return strIPAddess;
+    }
+}
