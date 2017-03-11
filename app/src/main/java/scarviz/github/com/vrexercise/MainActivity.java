ce06c55666bd6f2b916730b7f9b149a7ef162caf
Bizan Nishimura <lipoyang1453@gmail.com>
2017-03-11 19:11:52 +0900

    PWM制御メソッドの追加

--- app/src/main/java/scarviz/github/com/vrexercise/MainActivity.java
+++ app/src/main/java/scarviz/github/com/vrexercise/MainActivity.java
@@ -6,15 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.Pwm;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Hoge";
+    private static final int PULSE_PERIOD_MS = 20;
     TextView tv;
+    private Pwm mPwm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView)findViewById(R.id.ipadress);
+
+        PeripheralManagerService service = new PeripheralManagerService();
+        try {
+            mPwm = service.openPwm("PWM0");
+            mPwm.setPwmFrequencyHz(1000 / PULSE_PERIOD_MS);
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        }
     }
 
     @Override
@@ -29,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         try {
                             tv.setText(getIPAddress());
+                            setMotor(50);
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -50,4 +66,28 @@ public class MainActivity extends AppCompatActivity {
         Log.v("IP Address", strIPAddess);
         return strIPAddess;
     }
+
+    private void setMotor(int activePulseDuration) {
+        try {
+            mPwm.setPwmDutyCycle(activePulseDuration);
+            mPwm.setEnabled(true);
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close the PWM port.
+        Log.i(TAG, "Closing port");
+        try {
+            mPwm.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        } finally {
+            mPwm = null;
+        }
+    }
+
 }
